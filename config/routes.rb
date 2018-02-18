aa9fcbd4b8ea1c50c0437fc9bbe2640bb3e400f3
sonken625 <1113146c@gmail.com>
2018-02-19 01:11:47 +0900

    add search_key routing and controller for hct to be able to update search_key of query

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :queries
+  resources :request_messages
+  resources :query_definitions, param: :search_key do
+    resource :search_key ,only:[:update], module: "query_definitions"
+  end
+
   devise_for :hcts,:skip => [:sessions,:passwords]
   resources :firms
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
